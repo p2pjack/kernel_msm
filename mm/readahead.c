b801a34da7ac189072ec5ccd8e63dcf937aa7729
p2pjack <p2pjack@gmail.com>
2013-12-31 00:03:04 +0000

    some testing

--- mm/readahead.c
+++ mm/readahead.c
@@ -369,10 +369,10 @@ static int try_context_readahead(struct address_space *mapping,
 	size = count_history_pages(mapping, ra, offset, max);
 
 	/*
-	 * no history pages:
+	 * not enough history pages:
 	 * it could be a random read
 	 */
-	if (!size)
+	if (size <= req_size)
 		return 0;
 
 	/*
@@ -380,11 +380,11 @@ static int try_context_readahead(struct address_space *mapping,
 	 * it is a strong indication of long-run stream (or whole-file-read)
 	 */
 	if (size >= offset)
-		size *= 2;
+		size <<= 1;
 
 	ra->start = offset;
-	ra->size = get_init_ra_size(size + req_size, max);
-	ra->async_size = ra->size;
+	ra->size = min(size + req_size, max);
+        ra->async_size = 1;
 
 	return 1;
 }
